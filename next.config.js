243dc72982ef769239dc9f0e73bbcd391241bdfb
Elternheld <elternheld@gmail.com>
2025-04-26 17:00:25 +0200

    1

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     serverActions: true,
   },
-  reactStrictMode: true,
-  swcMinify: true,
+  // Hiermit wird NIE statisch gebaut!
+  dynamicRendering: true,
 };
 
 module.exports = nextConfig;
